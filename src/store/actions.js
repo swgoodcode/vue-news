8844a34840837bce6b14b2dda4880f1a2793f71e
SW <seoungwoo.h@gmail.com>
2020-03-06 05:20:50 +0900

    async await

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,30 +1,31 @@
 import {fetchItemInfo, fetchList, fetchUserInfo} from "../api";
 
 export default {
-    FETCH_USER({commit}, name) {
-        fetchUserInfo(name)
-            .then(({data}) => {
-                commit('SET_USER', data);
-            })
-            .catch(error => {
-                console.log(error);
-            })
+    async FETCH_USER({commit}, name) {
+        try {
+            const response = await fetchUserInfo(name);
+            commit('SET_USER', response.data);
+            return response;
+        } catch (error) {
+            console.log(error);
+        }
     },
-    FETCH_ITEM({commit}, item) {
-        fetchItemInfo(item)
-            .then(({data}) => {
-                commit('SET_ITEM', data);
-            })
-            .catch(error => {
-                console.log(error);
-            })
+    async FETCH_ITEM({commit}, item) {
+        try {
+            const response = await fetchItemInfo(item)
+            commit('SET_ITEM', response.data);
+            return response;
+        } catch (error) {
+            console.log(error);
+        }
     },
-    FETCH_LIST({commit}, pageName) {
-        return fetchList(pageName)
-            .then(({data}) => {
-                commit('SET_LIST', data);
-            }).catch(error => {
-                console.log(error);
-            })
+    async FETCH_LIST({commit}, pageName) {
+        try {
+            const response = await fetchList(pageName);
+            commit('SET_LIST', response.data);
+            return response;
+        } catch (error) {
+            console.log(error);
+        }
     }
 }
